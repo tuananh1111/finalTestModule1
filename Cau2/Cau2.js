320442ca5d0fd82debe492ef5f1a1ca143683089
tuananh1111 <nguyentuananhchuot0612@gmail.com>
2020-09-07 16:25:06 +0700

    update

--- Cau2/Cau2.js
+++ Cau2/Cau2.js
@@ -1,6 +1,9 @@
 function checkNumber(number, arr){
+    let count;
     for(let i=0; i<arr.length;i++){
         if(number==arr[i]){
+            count=i;
+            document.write("Vị trí thứ "+count);
             return true;
         }
     }return false;
